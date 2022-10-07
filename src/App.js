27c68a048781226144582a6cd3413ee41c57be4a
isabella <isacosta90a@gmail.com>
2022-10-07 16:27:08 -0300

    Função do signo libra

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ function App() {
   const[medio, setMedio]= useState(0);
   const[pequeno, setPequeno]= useState(0);
   const[desc,setDesc]= useState(0);
-  const[resul, setResul] = useState(0);
+  const[resul, setResul] = useState();
+
+  const [mes, setMes] = useState('');
+  const [dia, setDia] = useState();
+  const [resultado, setResultado] = useState();
 
   function Calcular(){
- 
-    
-
     let total = pequeno * 13.50  + medio * 15 + grande * 17.50;
     let desconto = total * desc/100
 
@@ -20,11 +21,20 @@ function App() {
     setResul(final);
     }
 
-      
+    function CalcularMes(mes, dia){
+      if (mes == 'Setembro' && dia >= 23 || mes == 'Outubro' && dia <= 22) {
+          return 'Sim'
+      }
+      else {
+          return 'Não'
+      }
+    }
+
+      function verificarClick() {
+  const resp = CalcularMes(mes, dia)
+  setResultado(resp);}
+
   
-
-    
-
     return(
     <div className="App">
       <h1>AÇAÍZINHO DA ISA E DA LARA</h1>
@@ -46,8 +56,23 @@ function App() {
           o resultado é {resul}
         </div>
       </div>
+      <div>
+      <h1>SIGNO LIBRA</h1>
+      <p>Mês</p>
+      <input type='text' value={ mes } onChange={e => setMes (e.target.value)} />
+
+      <p>Dia</p>
+      <input type='number' value={ dia }    onChange= {e => setDia (e.target.value)} />
+
+      <button onClick={verificarClick} >Verificar Signo</button>
+
+      <div>
+          <span>É do signo de Libra? {resultado}</span>
+      </div>
+    </div>
+    <h1>SORVETE DEIXA MINHA BOCA VERDE</h1>
     </div>
  );
-}
-
+    }
+  
 export default App;
