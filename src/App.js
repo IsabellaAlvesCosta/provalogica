c1b23033f5d16b84b88c4239af9bf7847befa9c5
isabella <isacosta90a@gmail.com>
2022-10-07 17:51:58 -0300

    css feito

--- src/App.js
+++ src/App.js
@@ -18,9 +18,7 @@ function App() {
 
   function Calcular(){
     let total = pequeno * 13.50  + medio * 15 + grande * 17.50;
-    let desconto = total * desc/100
-
-    
+    let desconto = total * desc/100    
     let final = total - desconto
     setResul(final);
     }
@@ -58,46 +56,50 @@ function App() {
   
     return(
     <div className="App">
+      <div className='acai'>
       <h1>AÇAÍZINHO DA ISA E DA LARA</h1>
-      <div>
         <div>
           Quantidade de açaí grande <input type='number'  value={grande} onChange={e => setGrande(Number(e.target.value))}/>
         </div>
+        <br></br>
         <div>
           Quantidade de açaí médio <input type='number'  value={medio} onChange={e => setMedio(Number(e.target.value))} />
         </div>
+        <br></br>
         <div>
-          Quantidade de açaí pequeno<input type='number'  value={pequeno} onChange={e => setPequeno(Number(e.target.value))}/>
+          Quantidade de açaí pequeno <input type='number'  value={pequeno} onChange={e => setPequeno(Number(e.target.value))}/>
         </div>
+        <br></br>
         <div>
-          Porcentagem de desconto <input type='number'  value={desc} onChange={e => setDesc(Number(e.target.value))}/>
+          Porcentagem de desconto <input type='number'  value={desc} onChange={e => setDesc(Number(e.target.value))}/><button onClick={Calcular} className='calcular-botao'>Calcular</button>
         </div>
-        <button onClick={Calcular}>Calcular</button>
-        <div>
-          o resultado é {resul}
-        </div>
+        
+        <span >
+          O resultado é {resul}
+        </span>
+      
       </div>
       <div>
       <h1>SIGNO LIBRA</h1>
-      <p>Mês</p>
-      <input type='text' value={ mes } onChange={e => setMes (e.target.value)} />
+      <div>
+        Mês <input type='text' value={ mes } onChange={e => setMes (e.target.value)} />
+      </div>
+      <br></br>
+      <div>
+        Dia <input type='number' value={ dia }    onChange= {e => setDia (e.target.value)} /> <button onClick={verificarClick} className='botao-signo'>
+        Verificar Signo
+        </button>
 
-      <p>Dia</p>
-      <input type='number' value={ dia }    onChange= {e => setDia (e.target.value)} />
-
-      <button onClick={verificarClick} >Verificar Signo</button>
-
+      </div>
       <div>
           <span>É do signo de Libra? {resultado}</span>
       </div>
     </div>
         <div>
                 <h1>SORVETE DEIXA MINHA BOCA VERDE</h1>
-                <p> Quantidade de gramas </p>
-                <input type='number' value={gramas} onChange={e => setGramas(e.target.value)} />
-
-                <button onClick={calcularClick}> calcular</button>
-
+                <div>
+                   Quantidade de gramas     <input type='number' value={gramas} onChange={e => setGramas(e.target.value)} /> <button onClick={calcularClick} className='calcular-botao'> Calcular</button>
+                </div>
                 <div>
                     <span>O total à pagar é R$ {resposta}  </span>
                 </div>
