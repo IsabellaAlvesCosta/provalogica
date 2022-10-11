b77d7903f41fab9a4ea041a387c9cad964a281a0
isabella <isacosta90a@gmail.com>
2022-10-11 15:18:41 -0300

    Função salário e distancia feitas, comec. temper.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 
 import { useEffect, useState } from 'react';
 import './App.css';
+import Acai from './assets/images/acai.png'
+import Libra from './assets/images/libraa.png'
+import Sorvete from './assets/images/picole.png'
 
 function App() {
   const[grande, setGrande]= useState(0);
@@ -24,6 +27,9 @@ function App() {
   const [consumo, setConsumo] = useState();
   const [distancia, setDistancia] = useState();
   const [totalParadas, setTotalParadas] = useState();
+
+  const [temperatura, setTemperatura] = useState();
+  const [resulTemperatura, setResulTemperatura] = useState();
 
   function Calcular(){
     let total = pequeno * 13.50  + medio * 15 + grande * 17.50;
@@ -81,14 +87,40 @@ function App() {
         setTotalParadas(resp);
       }
 
+      function calcularTemperatura(temp){
+        let msg = '';
+
+        if(temp >= 41){
+          msg = 'Hipertemia'
+        }
+        else if(temp >=39.6 && temp<41){
+          msg = 'Febre alta'
+        }
+        else if(temp>=37.6 && temp<39.6){
+          msg = 'febre'
+        }
+        else if(temp >=36 && temp<37.6){
+          msg='Normal'
+        }
+        else if(temp< 36) {
+          msg='hipotermia'
+        }
+        else{
+          msg='temperatura inválida'
+        }
+        return setResulTemperatura (msg);
+      }
+
   
     return(
     <div className="App">
       <div className='acai'>
       <h1>AÇAÍZINHO DA ISA E DA LARA</h1>
+      <img src={Acai} />
         <div>
           Quantidade de açaí grande <input type='number'  value={grande} onChange={e => setGrande(Number(e.target.value))}/>
         </div>
+        
         <br></br>
         <div>
           Quantidade de açaí médio <input type='number'  value={medio} onChange={e => setMedio(Number(e.target.value))} />
@@ -99,7 +131,7 @@ function App() {
         </div>
         <br></br>
         <div>
-          Porcentagem de desconto <input type='number'  value={desc} onChange={e => setDesc(Number(e.target.value))}/><button onClick={Calcular} className='calcular-botao'>Calcular</button>
+          Porcentagem de desconto <input type='number'  value={desc} onChange={e => setDesc(Number(e.target.value))}/> <button onClick={Calcular} className='calcular-botao'>Calcular</button>
         </div>
         
         <h3>
@@ -109,6 +141,7 @@ function App() {
       </div>
       <div>
       <h1>SIGNO LIBRA</h1>
+      <img src={Libra} />
       <div>
         Mês <input type='text' value={ mes } onChange={e => setMes (e.target.value)} />
       </div>
@@ -125,6 +158,7 @@ function App() {
     </div>
         <div>
                 <h1>SORVETE DEIXA MINHA BOCA VERDE</h1>
+                <img src={Sorvete} className='img-sorvete'/>
                 <div>
                    Quantidade de gramas     <input type='number' value={gramas} onChange={e => setGramas(e.target.value)} /> <button onClick={calcularClick} className='calcular-botao'> Calcular</button>
                 </div>
@@ -150,6 +184,14 @@ function App() {
         <button onClick={repost} className='calcular-botao'>Calcular</button>
         <h3>O total de paradas é {totalParadas}</h3>
         </div> 
+
+        <div>
+          <h1>TEMPERATURA</h1>
+          <p>Informe temperatura <input type='number' value={temperatura} onChange={e => setTemperatura(Number(e.target.value))} /></p>
+          <button onClick={calcularTemperatura} className='calcular-botao'>Calcular</button>
+          <h3>
+            {resulTemperatura}</h3>
+        </div>
     </div>
  );
     }
